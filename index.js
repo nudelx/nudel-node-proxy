9151aa95b0e0d56c428b48e8962a87af7fb54088
nudelx <nudelx@gmail.com>
2018-11-22 15:13:22 +0200

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello SamNext  from  bla'
+  res.send('Hello SamNext  from  bla')
 })
 
 var server = app.listen(process.env.PORT || 5000, function() {
