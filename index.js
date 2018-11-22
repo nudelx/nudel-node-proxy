b3d68e86750656c229e7ad329c61e73b01f885bb
nudelx <nudelx@gmail.com>
2018-11-22 15:07:11 +0200

    init

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res) {
+  res.send('Hello SamNext  from  bla'
+})
+
+var server = app.listen(8080, function() {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('The app listening at http://%s:%s', host, port)
+  console.log('HELLO SamNext !!!!')
+})
